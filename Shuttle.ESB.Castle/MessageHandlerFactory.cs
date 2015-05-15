5381a635d3a1f1029fb145e71b190f0213a33692
eben-roux <me@ebenroux.co.za>
2015-05-15 09:41:08 +0200

    - release v3.4.2
    - added message handler registration

--- Shuttle.ESB.Castle/MessageHandlerFactory.cs
+++ Shuttle.ESB.Castle/MessageHandlerFactory.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
+using System.Reflection;
 using Castle.MicroKernel.Registration;
 using Castle.Windsor;
 using Shuttle.Core.Infrastructure;
@@ -10,49 +10,64 @@ namespace Shuttle.ESB.Castle
 {
 	public class CastleMessageHandlerFactory : MessageHandlerFactory
 	{
-		private readonly IWindsorContainer container;
-
-		private static readonly Type generic = typeof(IMessageHandler<>);
-
-		private readonly List<Type> messageTypesHandled = new List<Type>();
+		private readonly IWindsorContainer _container;
+		private static readonly Type _generic = typeof (IMessageHandler<>);
+		private readonly Dictionary<Type, Type> _messageHandlerTypes = new Dictionary<Type, Type>();
+		private readonly ILog _log;
 
 		public CastleMessageHandlerFactory(IWindsorContainer container)
 		{
 			Guard.AgainstNull(container, "container");
 
-			this.container = container;
+			_container = container;
+
+			_log = Log.For(this);
 		}
 
 		public override IMessageHandler CreateHandler(object message)
 		{
-			var all = container.ResolveAll(generic.MakeGenericType(message.GetType()));
+			var all = _container.ResolveAll(_generic.MakeGenericType(message.GetType()));
 
-			return all.Length != 0 ? (IMessageHandler)all.GetValue(0) : null;
+			return all.Length != 0 ? (IMessageHandler) all.GetValue(0) : null;
 		}
 
 		public override IEnumerable<Type> MessageTypesHandled
 		{
-			get { return new ReadOnlyCollection<Type>(messageTypesHandled); }
+			get { return _messageHandlerTypes.Keys; }
 		}
 
 		public override void Initialize(IServiceBus bus)
 		{
 			Guard.AgainstNull(bus, "bus");
 
-			if (!container.Kernel.HasComponent(typeof(IServiceBus)))
+			if (!_container.Kernel.HasComponent(typeof (IServiceBus)))
 			{
-				container.Register(Component.For<IServiceBus>().Instance(bus));
+				_container.Register(Component.For<IServiceBus>().Instance(bus));
 			}
 
-			messageTypesHandled.Clear();
+			_messageHandlerTypes.Clear();
 
-			var handlers = container.Kernel.GetAssignableHandlers(typeof(IMessageHandler));
+			RefreshHandledTypes();
+		}
+
+		private void RefreshHandledTypes()
+		{
+			var handlers = _container.Kernel.GetAssignableHandlers(typeof (IMessageHandler<>));
 
 			foreach (var handler in handlers)
 			{
-				foreach (var type in handler.ComponentModel.Implementation.InterfacesAssignableTo(generic))
+				foreach (var type in handler.ComponentModel.Implementation.InterfacesAssignableTo(_generic))
 				{
-					messageTypesHandled.Add(type.GetGenericArguments()[0]);
+					var messageType = type.GetGenericArguments()[0];
+
+					if (_messageHandlerTypes.ContainsKey(messageType))
+					{
+						return;
+					}
+
+					_messageHandlerTypes.Add(messageType, type);
+
+					_log.Information(string.Format(ESBResources.MessageHandlerFactoryHandlerRegistered, messageType.FullName, type.FullName));
 				}
 			}
 		}
@@ -61,7 +76,31 @@ namespace Shuttle.ESB.Castle
 		{
 			base.ReleaseHandler(handler);
 
-			container.Release(handler);
+			_container.Release(handler);
+		}
+
+		public CastleMessageHandlerFactory RegisterHandlers()
+		{
+			foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+			{
+				RegisterHandlers(assembly);
+			}
+
+			return this;
+		}
+
+		public CastleMessageHandlerFactory RegisterHandlers(Assembly assembly)
+		{
+			_container.Register(
+				Classes.FromAssembly(assembly)
+					.BasedOn(typeof (IMessageHandler<>))
+					.WithServiceFromInterface()
+					.LifestyleTransient()
+				);
+
+			RefreshHandledTypes();
+
+			return this;
 		}
 	}
 }
